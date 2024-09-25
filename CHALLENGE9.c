058cc7b84ba7599db3f8837376daf6fbeb10b8a3
Amine-youcode <bourhymamine@gmail.com>
2024-09-25 19:23:29 +0200

    youcode

---
+++ CHALLENGE9.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main() {
+    char g ;
+  printf("entrer la lettre: ");
+  scanf("%c",&g);
+  if (g>=A && g=<Z)
+      printf("la lettreil des alphabets et majuscule ");
+  
+  else if (g>=a && g=<z)
+      printf("la condution minuscule et alphabets");
+  
+  else 
+      printf("systeme de youcode error");
+  
+   
+   
+   
+ return 0; 
+   
+}
