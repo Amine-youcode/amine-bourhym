7da60bad285c69e3ab35241e7341ba417d5718f4
Amine-youcode <bourhymamine@gmail.com>
2024-09-26 11:13:35 +0200

    teste1

---
+++ teste1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x , A = 1;
+    printf ("entrer une valeur X: ");
+    scanf ("%i",&x);
+  while (A <= 10 ){
+      printf ("%i * %i = %i \n",x,A,x*A);
+      A++;
+  }
+    
+
+    return 0;
+}
